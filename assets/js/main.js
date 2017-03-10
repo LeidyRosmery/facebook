edec67c4720274ffb804a95d00f21979f1389fee
LeidyRosmery <rosm3rita@hotmail.com>
2017-03-10 00:12:48 -0500

    creando divs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,24 @@
-/*
-
-function publicar(){
-  var span=document.createElement("div");
-  var parrafo=document.createElement("p");
-  var a=document.createElement("a");
-  span.innerHTML=publicacion.value + " <br>";
-  document.body.appendChild(span);
-
-}
-*/
-
-
 var publicacion= document.getElementById("publicacion");
 var publico= document.getElementById("postear");
+var arrayTexto=[];
+console.log(publicacion.value+"hgvm");
+arrayTexto.push(publicacion.value);
 function publicar() {
-  var div=document.createElement("div");
+  var field=document.createElement("fieldset");
   var p =document.createElement("p");
   var a1 =document.createElement("a");
   var a2 =document.createElement("a");
-/*  var p =document.createElement("p");
-  p.innerHTML= publicacion.value;
-  var a1 =document.createElement("a");
-  var a2 =document.createElement("a");
-  span.innerHTML=publicacion.value + " <br>";*/
-  p.innerHTML= publicacion.value;
-  publico.appendChild(div);
-  div.appendChild(p);
-  p.appendChild(a1).setAttribute( "href","editar");;
-  p.appendChild(a2);
-  console.log(div);
+  p.innerHTML= publicacion.value+ "<br>";
+  a1.innerHTML="Editar";
+  a2.innerHTML="Eliminar";
+  publico.appendChild(field);
+  field.appendChild(p);
+  p.appendChild(a1).setAttribute( "href","editar");
+  p.appendChild(a2).setAttribute("href","eliminar");
+  clearTex();
+  console.log(arrayTexto);
+}
+
+function clearTex(){
+  publicacion.value="";
 }
