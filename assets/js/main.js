85474c58aaf557f2f0941dd967ef0006c5e9c8d1
LeidyRosmery <rosm3rita@hotmail.com>
2017-03-10 00:45:14 -0500

    ingresando botones a cada publicacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,32 @@
 var publicacion= document.getElementById("publicacion");
 var publico= document.getElementById("postear");
 var arrayTexto=[];
-console.log(publicacion.value+"hgvm");
-arrayTexto.push(publicacion.value);
 function publicar() {
+  arrayTexto.push(publicacion.value);
   var field=document.createElement("fieldset");
   var p =document.createElement("p");
-  var a1 =document.createElement("a");
-  var a2 =document.createElement("a");
+  //var a1 =document.createElement("a");
+  //var a2 =document.createElement("a");
+  var b1=document.createElement("button").setAttribute("onclick","editar()");
+  var b2=document.createElement("button");
   p.innerHTML= publicacion.value+ "<br>";
-  a1.innerHTML="Editar";
-  a2.innerHTML="Eliminar";
+  //a1.innerHTML="Editar";
+  b1.innerHTML="editar";
+  b2.innerHTML="eliminar";
+  //a2.innerHTML="Eliminar";
+
   publico.appendChild(field);
   field.appendChild(p);
-  p.appendChild(a1).setAttribute( "href","editar");
-  p.appendChild(a2).setAttribute("href","eliminar");
+  p.appendChild(b1);
+  p.appendChild(b2);
+//  p.appendChild(a1).setAttribute( "href","editar");
+//  p.appendChild(a2).setAttribute("href","eliminar");
   clearTex();
   console.log(arrayTexto);
 }
-
+function editar(){
+  console.log("desea editar el texto");
+}
 function clearTex(){
   publicacion.value="";
 }
